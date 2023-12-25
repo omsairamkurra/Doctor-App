4888ae35127d8fb886f0c3b85823e63cec9dbaa9
omsairamkurra <kurraomsairam2@gmail.com>
2023-12-25 19:58:02 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const { connectDB } = require("./config/db.js");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -14,6 +15,11 @@ app.use(morgan("dev"));
 app.use("/api/v1/user", require("./routes/userRoute"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 app.use("/api/v1/doctor", require("./routes/doctorRoute"));
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
